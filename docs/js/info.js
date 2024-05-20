2814a03ba0c54e36140f4e0be6d9fffee715a94a
Tomasi1609 <profesoredwin11@gmail.com>
2024-05-19 22:41:41 -0500

    Añandiendo parte del cronograma

--- docs/js/info.js
+++ docs/js/info.js
@@ -1,4 +1,4 @@
-let baseDatosCrono = [];
+let baseDatosCrono, cronoBD, backupCronoBD = [];
 let varHoraInicio, varObservacion;
 /*  Arreglo con la información del Cronograma
     Para el dato mesDia tener en cuenta que el orden (m/d/aaaa) - Enero = 0; Diciembre = 11
@@ -6,29 +6,78 @@ let varHoraInicio, varObservacion;
 */
 baseDatosCrono = [
     {
-    mesDia : '5620245',
-    tipoEvento : "3",
-    evento : 'Cumpleaños profesor Edwin Vargas Galvis',
-    horaInicio : '',
-    observacion : '',
-    },
-    {
-    mesDia : '41620244',
+    mesDia : '42020244',
     tipoEvento : "2",
-    evento : 'Día de la Santandereanidad',
+    evento : 'REUNIÓN CONSEJO DIRECTIVO',
     horaInicio : '',
-    observacion : 'Responsable: Grado Undécimo',
+    observacion : 'RECTORÍA',
     },
     {
-    mesDia : '62320246',
-    tipoEvento : "1",
-    evento : 'Actividad familiar',
+    mesDia : '43020244',
+    tipoEvento : "2",
+    evento : 'REUNIÓN CONSEJO ESTUDIANTIL',
     horaInicio : '',
-    observacion : '',
-        }
+    observacion : 'CONSEJO ESTUDIANTIL, PERSONERO',
+    },
+    {
+    mesDia : '5520245',
+    tipoEvento : "2",
+    evento : 'DIRECCIÓN DE GRADO',
+    horaInicio : '',
+    observacion : 'DOCENTES Y DIRECTIVOS',
+    },
+    {
+    mesDia : '5620245',
+    tipoEvento : "2",
+    evento : 'DÍA DE LA FAMILIA',
+    horaInicio : '',
+    observacion : 'DOCENTES Y DIRECTIVOS, ESTUDIANTES',
+    },
+    {
+    mesDia : '5720245',
+    tipoEvento : "2",
+    evento : 'JUNTA  TÉCNICA',
+    horaInicio : '',
+    observacion : 'RECTORÍA',
+    },
+    {
+        mesDia : '51220245',
+        tipoEvento : "2",
+        evento : 'REUNIÓN CONSEJO DIRECTIVO',
+        horaInicio : '',
+        observacion : 'RECTORÍA',
+        },
+        {
+            mesDia : '51320245',
+            tipoEvento : "2",
+            evento : 'DÍA DEL ESTUDIANTE',
+            horaInicio : '',
+            observacion : 'COMUNIDAD EDUCATIVA',
+            },
+            {
+                mesDia : '51720245',
+                tipoEvento : "2",
+                evento : 'ANÁLISIS DE COMPORTAMIENTO. Y COMITÉ DE CONVIVENCIA',
+                horaInicio : '',
+                observacion : 'DIRECTORES DE GRADO',
+                },
+                {
+                    mesDia : '51820245',
+                    tipoEvento : "2",
+                    evento : 'REUNIÓN DE COMISIÓN PROMOCIÓN Y EVALUACIÓN SEGUNDO PERIODO',
+                    horaInicio : '',
+                    observacion : 'COORDINACIÓN, DOCENTE PRIMARIA Y BACHILLERATO',
+                    },
+                    {
+                        mesDia : '51920245',
+                        tipoEvento : "2",
+                        evento : 'CARGUE DE NOTAS EN PLATAFORMA. Y REUNIÓN CONSEJO ACADÉMICO',
+                        horaInicio : '',
+                        observacion : 'DOCENTES Y DIRECTIVOS',
+                        }
+                        
 
 ]
-
 
 let validacion = JSON.parse(localStorage.getItem('baseDatosInfo'));
 if (validacion === null) {
